0e1f59ec7273f7e61cc8e28f1c0aaf3bdb3612cf
leanbs <davin.yu1110@gmail.com>
2019-07-09 01:43:58 +0700

    auto layout

--- SocialAcc/ViewController.swift
+++ SocialAcc/ViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 class ViewController: UITableViewController {
     fileprivate var cellId = "cellId"
     fileprivate var socialAccounts: [SocialAccount] = [
-        SocialAccount(title: "Instagram", url: "https://www.instagram.com/davinyii", imageUrl: "instagram"),
+        SocialAccount(title: "Github", url: "https://github.com/leanbs", imageUrl: "github"),
         SocialAccount(title: "Twitter", url: "https://www.twitter.com/davinyii", imageUrl: "twitter"),
-        SocialAccount(title: "Facebook", url: "https://www.facebook.com/davinyii", imageUrl: "facebook"),
+        SocialAccount(title: "Gmail", url: "davin.yu1110@gmail.com", imageUrl: "gmail"),
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,18 +32,19 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
+        return 60
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SocialAccountCell
         let socialAccount = socialAccounts[indexPath.row]
         cell.socialAccount = socialAccount
-//        cell.textLabel?.text = socialAccount.title
-//        cell.textLabel?.backgroundColor = .clear
+        //        cell.textLabel?.text = socialAccount.title
+        //        cell.textLabel?.backgroundColor = .clear
         return cell
     }
-
-
+    
+    
 }
+
 
--- SocialAcc/Views/SocialAccountCell.swift
+++ SocialAcc/Views/SocialAccountCell.swift
@@ -12,15 +12,49 @@ class SocialAccountCell: UITableViewCell {
     var socialAccount: SocialAccount? {
         didSet {
             guard let socialAccount = socialAccount else { return }
-            print(socialAccount.title)
-            self.textLabel?.text = socialAccount.title
-            self.textLabel?.backgroundColor = .clear
+            logoImageView.image = UIImage(named: socialAccount.imageUrl)
+            appTitle.text = socialAccount.title
+            appLink.setTitle(socialAccount.url, for: .normal)
+            appLink.setTitleColor(.black, for: .normal)
         }
     }
+    
+    fileprivate let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    fileprivate let appTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        return label
+    }()
+    
+    fileprivate let appLink = UIButton()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        contentView.backgroundColor = .yellow
+        let stack = UIStackView(arrangedSubviews: [appTitle, appLink])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        
+        addSubview(logoImageView)
+        addSubview(stack)
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        logoImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        logoImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        
+        logoImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        stack.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        stack.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        stack.leftAnchor.constraint(equalTo: logoImageView.rightAnchor, constant: 8).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
